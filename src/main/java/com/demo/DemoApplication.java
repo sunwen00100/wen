0adacc216d386a3e7064b17e879fa28e28260389
sunwen00100 <54738460@qq.com>
2018-09-05 10:18:52 +0800

    测试1

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
